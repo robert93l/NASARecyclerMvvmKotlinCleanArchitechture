e35d2a598a6de5680813e7cef3d03021d267a76f
roberto <roberto93lazaro@gmail.com>
2023-02-08 22:56:50 -0600

    refactor adapter

--- app/src/main/java/com/example/nasarecyclermvvmkotlincleanarchitechture/ui/RoverPhotosAdapter.kt
+++ app/src/main/java/com/example/nasarecyclermvvmkotlincleanarchitechture/ui/RoverPhotosAdapter.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.nasarecyclermvvmkotlincleanarchitechture.R
 import com.example.nasarecyclermvvmkotlincleanarchitechture.data.Photo
+import org.w3c.dom.Text
 
 
 class RoverPhotosAdapter() :
@@ -32,13 +34,27 @@ class RoverPhotosAdapter() :
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val ivPhoto: ImageView = itemView.findViewById(R.id.iv_photo)
+        private val tvcamera: TextView = itemView.findViewById(R.id.camera_name)
+        private val tverath:  TextView = itemView.findViewById(R.id.earth_date)
+        private val tvrover:  TextView = itemView.findViewById(R.id.rover_name)
+        private val tvlanding:  TextView = itemView.findViewById(R.id.landing_date)
+        private val tvlaunch:  TextView = itemView.findViewById(R.id.launch_date)
+        private val tvstatus:  TextView = itemView.findViewById(R.id.status)
 
         fun bind(photo: Photo) {
             Glide.with(ivPhoto.context)
                 .load(photo.img_src)
-                .override(200, 200)
+                .override(300, 300)
                 .into(ivPhoto)
+
+            tvcamera.text = "Camera: "+photo.camera.name
+            tverath.text = "Earth date: " + photo.earth_date
+            tvrover.text = "Name Rover: "+photo.rover.name
+            tvlanding.text = "Landing "+photo.rover.landing_date
+            tvlaunch.text = "Launch "+photo.rover.launch_date
+            tvstatus.text = "Status: "+photo.rover.status
         }
+
     }
 
     class DiffUtilCallBack: DiffUtil.ItemCallback<Photo>() {
@@ -48,6 +64,7 @@ class RoverPhotosAdapter() :
 
         override fun areContentsTheSame(oldItem: Photo, newItem: Photo): Boolean {
             return  oldItem.id == newItem.id
+                    && oldItem.rover.id == oldItem.rover.id
 
         }
     }
