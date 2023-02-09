228539c0a0d9e6171f5dc57b0e3bd6b320b9a8ec
roberto <roberto93lazaro@gmail.com>
2023-02-09 16:38:48 -0600

    refactor adapter

--- app/src/main/java/com/example/nasarecyclermvvmkotlincleanarchitechture/ui/RoverPhotosAdapter.kt
+++ app/src/main/java/com/example/nasarecyclermvvmkotlincleanarchitechture/ui/RoverPhotosAdapter.kt
@@ -2,25 +2,23 @@ package com.example.nasarecyclermvvmkotlincleanarchitechture.ui
 
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.nasarecyclermvvmkotlincleanarchitechture.R
 import com.example.nasarecyclermvvmkotlincleanarchitechture.data.Photo
-import org.w3c.dom.Text
+import com.example.nasarecyclermvvmkotlincleanarchitechture.databinding.RoverItemLayoutBinding
+
 
 
 class RoverPhotosAdapter() :
     PagingDataAdapter<Photo, RoverPhotosAdapter.ViewHolder>(DiffUtilCallBack()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.rover_item_layout, parent, false)
-        return ViewHolder(view)
+        val binding = RoverItemLayoutBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -28,33 +26,25 @@ class RoverPhotosAdapter() :
         if (item != null) {
             holder.bind(item)
         }
-
     }
 
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val ivPhoto: ImageView = itemView.findViewById(R.id.iv_photo)
-        private val tvcamera: TextView = itemView.findViewById(R.id.camera_name)
-        private val tverath:  TextView = itemView.findViewById(R.id.earth_date)
-        private val tvrover:  TextView = itemView.findViewById(R.id.rover_name)
-        private val tvlanding:  TextView = itemView.findViewById(R.id.landing_date)
-        private val tvlaunch:  TextView = itemView.findViewById(R.id.launch_date)
-        private val tvstatus:  TextView = itemView.findViewById(R.id.status)
+    inner class ViewHolder(private val binding: RoverItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(photo: Photo) {
-            Glide.with(ivPhoto.context)
+            Glide.with(binding.ivPhoto.context)
                 .load(photo.img_src)
                 .override(300, 300)
-                .into(ivPhoto)
+                .into(binding.ivPhoto)
 
-            tvcamera.text = "Camera: "+photo.camera.name
-            tverath.text = "Earth date: " + photo.earth_date
-            tvrover.text = "Name Rover: "+photo.rover.name
-            tvlanding.text = "Landing "+photo.rover.landing_date
-            tvlaunch.text = "Launch "+photo.rover.launch_date
-            tvstatus.text = "Status: "+photo.rover.status
+            binding.cameraName.text = "Camera: "+photo.camera.name
+            binding.earthDate.text = "Earth date: " + photo.earth_date
+            binding.roverName.text = "Name Rover: "+photo.rover.name
+            binding.landingDate.text = "Landing: "+photo.rover.landing_date
+            binding.launchDate.text =  "Launch: "+ photo.rover.launch_date
+            binding.status.text = "Status: "+photo.rover.status.uppercase()
+            binding.urkLink.text = "URL: " + photo.img_src
         }
-
     }
 
     class DiffUtilCallBack: DiffUtil.ItemCallback<Photo>() {
@@ -65,7 +55,6 @@ class RoverPhotosAdapter() :
         override fun areContentsTheSame(oldItem: Photo, newItem: Photo): Boolean {
             return  oldItem.id == newItem.id
                     && oldItem.rover.id == oldItem.rover.id
-
         }
     }
 }
